0843144294e288a8415bb6ffea019f4edfd90184
santiagoinostroza <santiagoinostroza2@gmail.com>
2022-08-10 23:41:17 -0400

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import App from './layouts/app.vue';
+
+const app = createApp(App);
+app.mount('#app');
+
---
+++ resources/js/layouts/app.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        Laravel 9 con vue 3 y vite. js 
+    </div>
+</template>
